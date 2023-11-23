2856e1f30fc0bc62a6a33045e24a2ebbb3aae4bb
LuciaPuppo897 <alerome897@gmail.com>
2023-11-23 14:15:36 -0800

    feat: change all method to call dbstorage properly

--- console.py
+++ console.py
@@ -219,7 +219,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -235,15 +235,16 @@ class HBNBCommand(cmd.Cmd):
 
         if args:
             args = args.split(' ')[0]  # remove possible trailing args
-            if args not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-                return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
+        if args not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        obj_dict = storage.all(cls=HBNBCommand.classes[args])
+        for k, v in obj_dict.items():
+            print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
-                print_list.append(str(v))
+            obj_dict = storage.all()
+        for k, v in obj_dict.items():
+            print_list.append(str(v))
 
         print(print_list)
 
