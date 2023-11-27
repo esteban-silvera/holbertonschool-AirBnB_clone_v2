893c2c13a0f09fe49f39dea276200698208a0795
JoseMilans <josemilans@gmail.com>
2023-11-27 20:07:04 +0000

    refactor: Update Amenity model for many-to-many relationship with Place

--- models/amenity.py
+++ models/amenity.py
@@ -3,11 +3,17 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
     """Amenities of a place"""
     __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary='place_amenity',
-                                   viewonly=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary=place_amenity,
+                                       back_populates="amenities",
+                                       viewonly=False)
+    else:
+        name = ""
