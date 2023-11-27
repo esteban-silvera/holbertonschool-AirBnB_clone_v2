aaa035e8d8ccfb37065f7fdd8f65d17d9d848a0d
JoseMilans <josemilans@gmail.com>
2023-11-27 19:34:14 +0000

    feat: Add many-to-many relationship with Place

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os
 
 
 class Amenity(BaseModel, Base):
-   """Amenities of a place"""
-   __tablename__ = "amenities"
-   name = Column(String(128), nullable=False)
-   # place_amenities = relationship('place', secondary='place_amenity',
-                                  # nullable=False)
- 
+    """Amenities of a place"""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary='place_amenity',
+                                   viewonly=False)
