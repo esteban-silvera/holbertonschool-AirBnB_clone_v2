cf91bb601659a73ffdc4061ce48d8bbb6323e348
JoseMilans <josemilans@gmail.com>
2023-11-28 03:01:30 +0000

    fix: resolve initialization issues in BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -20,17 +20,23 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if kwargs:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.updated_at = datetime.utcnow()
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.created_at = datetime.utcnow()
             kwargs.pop('__class__', None)  # remove class attr
             self.__dict__.update(kwargs)
         else:
             # initialise with default values if no kwargs
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.utcnow()
-            models.storage.new(self)
+        models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
