114537ccc138a78dd71120f982da98a5f0e347d5
LuciaPuppo897 <alerome897@gmail.com>
2023-12-16 09:55:36 -0800

    add task 0 y 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3}
 """
-Task 0 flask web
+Task 1 flask web
 """
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
@@ -13,6 +12,11 @@ app = Flask(__name__)
 def Hello_hbnb():
     """returns hello hbnh!"""
     return "Hello HBNB!"
+
+
+def hbnb():
+    """returns hbnb"""
+    return "HBNB"
 
 
 if __name__ == "__main__":
