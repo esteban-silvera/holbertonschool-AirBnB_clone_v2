c16e16582d058abd48cf667cf30d7aea5e5a2f04
esteban-silvera <estebansilvera000@gmail.com>
2024-03-06 03:29:35 -0300

    7 fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,24 +4,21 @@
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """coment"""
+    """cometns"""
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """coment"""
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda x: x.name)
-    return render_template('states_list.html', states=states)
+    states = sorted(storage.all("State").values(), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
