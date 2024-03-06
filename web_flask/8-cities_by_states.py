90a6a518da043c03e536581f85cd626858d8d9b4
esteban-silvera <estebansilvera000@gmail.com>
2024-03-06 03:47:54 -0300

    .\8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,24 +2,24 @@
 """coments"""
 
 
-from flask import Flask, render_template
-from ..models import storage, State
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Display a HTML page with the states and their cities"""
-    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Close the storage on teardown"""
+    """closes the storage on teardown"""
     storage.close()
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
