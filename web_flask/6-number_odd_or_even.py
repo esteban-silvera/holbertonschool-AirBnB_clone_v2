5d59eb85fae4dbe6f02ac20ce4a4acc2f490145a
esteban-silvera <estebansilvera000@gmail.com>
2024-03-06 03:00:57 -0300

    fix styles

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -50,10 +50,10 @@ def display_number_odd_or_even(n):
     """cometns"""
     if n % 2 == 0:
         return render_template("6-number_odd_or_even.html",
-        n=n, odd_or_even="even")
+                               n=n, odd_or_even="even")
     else:
         return render_template("6-number_odd_or_even.html",
-        n=n, odd_or_even="odd")
+                               n=n, odd_or_even="odd")
 
 
 if __name__ == "__main__":
