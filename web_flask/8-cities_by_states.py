21b0f7b9f67cb70d0125005afca06f92718312bb
esteban-silvera <estebansilvera000@gmail.com>
2024-03-06 03:43:12 -0300

    .\8-cities_by_states.py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""coments"""
+
+
+from flask import Flask, render_template
+from models import storage, State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with the states and their cities"""
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close the storage on teardown"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
